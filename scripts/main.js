4b05cf6342ef30faafbf3aa16aa61913939c0906
Edddie3d <Eduard.chernikh@gmail.com>
2021-03-20 07:49:13 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 (function ($) {
-    let authToken;
     let webViewInterface = window.nsWebViewInterface;
 
     webViewInterface.on('loadDataResponse', onDataLoaded);
@@ -7,11 +6,15 @@
     webViewInterface.on('authorized', authorizedEventHandler);
     webViewInterface.on('unauthorized', unauthorizedEventHandler);
 
+    webViewInterface.on('updateOrderToggle', updateToggleEventHandler);
+
 
     let enableOrderPicker = false;
+    let filtersList;
     $(function () {
         $('#login-button').on('click', login);
-        $('#toggler').on('click', toggleOrderPicker);
+        $('#orderToggle').on('click', toggleOrderPicker);
+        $('#geoToggle').on('click', toggleGeoPicker);
         webViewInterface.emit('loadData')
     });
 
@@ -21,6 +24,7 @@
         let password = $('#password').val();
         webViewInterface.emit('login', {login, password});
     }
+
     function toggleOrderPicker(e) {
         e.preventDefault();
         if (enableOrderPicker) {
@@ -31,9 +35,23 @@
             $('.toggler .toggle').html('Вкл');
         }
         enableOrderPicker = !enableOrderPicker;
+        webViewInterface.emit('updateOrderToggle', {enabled: enableOrderPicker});
     }
+
+    function toggleGeoPicker(e) {
+
+    }
+
     function addFilter(e) {
+        // TODO: hide modal window
+        // let value = $("#filterInput").val();
+        // $("#filterInput").val('');
+        // value = value.trim();
+        // if(value.length < 3) return;
+        // filtersList.push(value);
+        webViewInterface.emit('updateFilters', {filters: filtersList.join(';')});
     }
+
     function removeFilter(e) {
     }
 
@@ -41,13 +59,32 @@
     function onDataLoaded(data) {
         $('#login').val(data.login);
         $('#password').val(data.password);
+        if (data.filters.length === 0) {
+            filtersList = [];
+        } else {
+            filtersList = data.filters.split(';');
+        }
     }
+
     function authorizedEventHandler(data) {
-        authToken = data.auth;
         $('#login').val('');
-        $('#password').val(data.auth);
+        $('#password').val('');
+        $('.screen.sign-in').hide();
+        $('.screen.main').show();
     }
+
     function unauthorizedEventHandler(data) {
         $('#password').val('');
+    }
+
+    function  updateToggleEventHandler(data) {
+        enableOrderPicker = data.enabled;
+        if (enableOrderPicker) {
+            $('.toggler').removeClass('on').addClass('off');
+            $('.toggler .toggle').html('Выкл');
+        } else {
+            $('.toggler').removeClass('off').addClass('on');
+            $('.toggler .toggle').html('Вкл');
+        }
     }
 })(jQuery)
